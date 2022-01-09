20c1a2c58a41fb2ec728d2b3417a3bdf82f8c32c
anastasiabaran <barananastasia14@gmail.com>
2022-01-09 18:25:14 +0000

    pzdc

---
+++ learn-react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Header.module.css';
+
+const Header = (props) => {
+ return <header className = {classes.header}>
+        <img src = "https://www.meme-arsenal.com/memes/b0c73f50f74400e6458644eac56fa9f5.jpg"/>
+      </header>
+}
+
+export default Header
