3df7ebbde1f8650877fd60a6baef806ee5be03af
anastasiabaran <barananastasia14@gmail.com>
2022-01-11 22:58:25 +0000

    react-video-29

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -3,30 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
-let posts = [
-  {id: 1, message: "Lime", likesCount: 24},
-  {id: 2, message: "Cherry", likesCount: 16},
-  {id: 3, message: "Mango", likesCount: 42}
-]
-
-let dialogs = [
-  {id: 1, name: 'John'},
-  {id: 2, name: 'Paul'},
-  {id: 3, name: 'James'},
-  {id: 4, name: 'Anderson'}
-]
-
-let messages = [
-  {id: 1, message: 'Hi'},
-  {id: 2, message: 'How your new tastes pepsi?'},
-  {id: 3, message: 'It is so good!'}
-]
+import state from './redux/state'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts = {posts} dialogs = {dialogs} messages = {messages}/>
+    <App state = {state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ learn-react/src/redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage : {
+        posts : [
+            {id: 1, message: "Lime", likesCount: 24},
+            {id: 2, message: "Cherry", likesCount: 16},
+            {id: 3, message: "Mango", likesCount: 42}
+          ]
+    },
+
+    dialogsPage : {
+        dialogs : [
+            {id: 1, name: 'John'},
+            {id: 2, name: 'Paul'},
+            {id: 3, name: 'James'},
+            {id: 4, name: 'Anderson'}
+          ],
+          
+          messages : [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How your new tastes pepsi?'},
+            {id: 3, message: 'It is so good!'}
+          ]
+    }
+      
+     
+}
+
+export default state
