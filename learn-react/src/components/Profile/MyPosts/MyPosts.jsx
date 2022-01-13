906fc22ce67ab0a1c6da580475782d1d573a06b8
anastasiabaran <barananastasia14@gmail.com>
2022-01-13 10:29:34 +0000

    react-video-21

--- learn-react/src/components/Profile/MyPosts/MyPosts.jsx
+++ learn-react/src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,22 @@ const MyPosts = (props) => {
 
 let postsElements = props.posts.map(p => <Post taste={p.message} likesCount={p.likesCount} /> )
 
+let newPostElement = React.useRef();
+
+let addPost = () => {
+  let text = newPostElement.current.value;
+  alert(text) 
+}
+
   return (
     <div className={classes.postsBlock}>
       <h3>My post</h3>
       <div>
         <div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
       <div>
       </div>
-        <button>Add post</button>
+        <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={classes.post}>
